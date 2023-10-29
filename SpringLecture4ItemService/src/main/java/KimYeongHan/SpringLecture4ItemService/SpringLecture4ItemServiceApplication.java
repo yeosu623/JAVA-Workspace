a4fca8a9c3fbb3b58e7ffd9e9161feec4fca48e1
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2023-10-30 00:02:33 +0900

    code update.

---
+++ SpringLecture4ItemService/src/main/java/KimYeongHan/SpringLecture4ItemService/SpringLecture4ItemServiceApplication.java
@@ -0,0 +1,13 @@
+package KimYeongHan.SpringLecture4ItemService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLecture4ItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLecture4ItemServiceApplication.class, args);
+	}
+
+}
---
+++ SpringLecture4ItemService/src/test/java/KimYeongHan/SpringLecture4ItemService/SpringLecture4ItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package KimYeongHan.SpringLecture4ItemService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLecture4ItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
