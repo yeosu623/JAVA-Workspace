398271f204e238a7d8aa737bd114357665519d52
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2023-11-12 22:04:19 +0900

    code update.

---
+++ SpringLecture5ThymeleafBasic/src/main/java/KimYeongHan/SpringLecture5ThymeleafBasic/SpringLecture5ThymeleafBasicApplication.java
@@ -0,0 +1,13 @@
+package KimYeongHan.SpringLecture5ThymeleafBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLecture5ThymeleafBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLecture5ThymeleafBasicApplication.class, args);
+	}
+
+}
---
+++ SpringLecture5ThymeleafBasic/src/test/java/KimYeongHan/SpringLecture5ThymeleafBasic/SpringLecture5ThymeleafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package KimYeongHan.SpringLecture5ThymeleafBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLecture5ThymeleafBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
