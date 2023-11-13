f4027294465dc26db2d5e807a27cdcbaf9177ce7
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2023-11-14 02:23:22 +0900

    code update.

---
+++ SpringLecture5Validation/src/main/java/KimYeongHan/SpringLecture5Validation/SpringLecture5ValidationApplication.java
@@ -0,0 +1,13 @@
+package KimYeongHan.SpringLecture5Validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLecture5ValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLecture5ValidationApplication.class, args);
+	}
+
+}
---
+++ SpringLecture5Validation/src/test/java/KimYeongHan/SpringLecture5Validation/SpringLecture5ValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package KimYeongHan.SpringLecture5Validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLecture5ValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
