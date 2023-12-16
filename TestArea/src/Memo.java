3e92d091c5916c6c7d2dbd350c542681049145a6
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2023-12-17 00:28:11 +0900

    code update.

--- TestArea/src/Memo.java
+++
@@ -1,6 +0,0 @@
-public class MemberService {
-    // 다형성이란 속성을 이용하게 되면, 이렇게 하나의 참조변수에 원하는 객체를 생성하여 연결할 수 있다.
-
-    //private MemberRepository memberRepository = new MemoryMemberRepository();
-    private MemberRepository memberRepository = new JdbcMemberRepository();
-}
