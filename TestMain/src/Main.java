d92b5d392889006ae1043767b940ec010202c540
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-04-22 01:30:35 +0900

    code update.

--- TestMain/src/Main.java
+++ TestMain/src/Main.java
@@ -1,89 +1,20 @@
 // 21911981 정수열
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-
-class Node {
-    int key;
-    Node left, right;
-
-    public Node(int key) {
-        this.key = key;
-    }
-}
-
-class BST {
-    public Node root;
-
-    protected Node treeSearch(int key) {
-        Node x = root;
-
-        while(true) {
-            int cmp = key - x.key;
-
-            if(cmp == 0) return x;
-            else if(cmp < 0) {
-                if(x.left == null) return x;
-                else x = x.left;
-            }
-            else {
-                if(x.right == null) return x;
-                else x = x.right;
-            }
-        }
-    }
-
-    public void put(int key) {
-        if(root == null) {
-            root = new Node(key);
-            return;
-        }
-
-        Node x = treeSearch(key);
-        int cmp = key - x.key;
-
-        Node newNode = new Node(key);
-        if(cmp < 0) x.left = newNode;
-        else x.right = newNode;
-    }
-
-    public void postorder(Node x, ArrayList<Integer> keyList) {
-        if(x != null) {
-            postorder(x.left, keyList);
-            postorder(x.right, keyList);
-            keyList.add(x.key);
-        }
-    }
-}
+import java.io.*;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        StringTokenizer st;
-        String str;
-        BST bst = new BST();
+        String s = sc.next();
 
-        while((str = br.readLine()) != null)
-        {
-            st = new StringTokenizer(str, " ");
-            int input = Integer.parseInt(st.nextToken());
-            bst.put(input);
-        }
+        Set<String> set = new HashSet<>();
+        for(int i = 0; i < s.length(); i++)
+            for(int j = 1; j <= s.length() - i; j++)
+                set.add(s.substring(i, i+j));
 
-        ArrayList<Integer> answer = new ArrayList<>();
-        bst.postorder(bst.root, answer);
-
-        for(int i = 0; i < answer.size(); i++)
-            bw.write(answer.get(i).toString() + "\n");
-
-        br.close();
-        bw.flush();
-        bw.close();
+        System.out.println(set.size());
     }
 }
