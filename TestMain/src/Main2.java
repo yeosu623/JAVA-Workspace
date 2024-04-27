174e70018ea60f1a3c6cf58e93a8a6978d5dd4bf
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-04-27 21:48:35 +0900

    code update.

--- TestMain/src/Main2.java
+++ TestMain/src/Main2.java
@@ -1,14 +1,29 @@
-import java.util.HashMap;
 
+class A {
+    int a, b;
+    public A(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int get() {
+        return a+b;
+    }
+}
+
+class B extends A {
+    int t;
+    public B(int t) {
+        super(t, t+1);
+    }
+
+    public int get() {
+        return t*t;
+    }
+}
 public class Main2 {
     public static void main(String[] args) {
-
-        HashMap<String, String> h1 = new HashMap<>();
-        h1.put("aaa", "1111");
-        h1.put("bbb", "2222");
-        h1.put("ccc", "3333");
-
-        System.out.println(h1.get("ddd"));
+        A a = new B(3);
     }
 }
 
