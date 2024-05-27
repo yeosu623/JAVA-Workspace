467d1f63db812b8b667978a0bbbc56fc1e3f5341
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-05-28 00:25:04 +0900

    code update.

--- TestMain/src/HW2.java
+++ TestMain/src/HW2.java
@@ -1,35 +1,19 @@
 // 21911981 정수열
-import java.util.Scanner;
+import java.util.*;
 
-public class HW2 {
+class Solution {
+    public boolean solution(String[] phone_book) {
 
-    public static void main(String[] args) {
-        int[][] cache = new int[1000][1000];
-        Scanner sc = new Scanner(System.in);
-        String s1 = sc.nextLine();
-        String s2 = sc.nextLine();
+        Arrays.sort(phone_book);
+        for(int i = 0; i < phone_book.length - 1; i++) {
+            String compare = phone_book[i];
+            String target = phone_book[i+1];
 
-        for(int i = 0; i < s1.length(); i++)
-            for(int j = 0; j < s2.length(); j++) {
-                if(s1.charAt(i) == s2.charAt(j)) cache[i+1][j+1] = cache[i][j] + 1;
-                else cache[i+1][j+1] = Math.max(cache[i+1][j], cache[i][j+1]);
-            }
-
-        int y = s1.length();
-        int x = s2.length();
-        String s = "";
-        while(cache[y][x] != 0) {
-            if(cache[y][x] == cache[y-1][x]) y--;
-            else if(cache[y][x] == cache[y][x-1]) x--;
-            else {
-                s += s1.charAt(y-1);
-                y--;
-                x--;
-            }
+            if(compare.length() <= target.length())
+                if(target.substring(0, compare.length()).equals(compare))
+                    return false;
         }
-        s = new StringBuilder(s).reverse().toString();
 
-        System.out.println(s);
-        System.out.println(cache[s1.length()][s2.length()]);
+        return true;
     }
 }
