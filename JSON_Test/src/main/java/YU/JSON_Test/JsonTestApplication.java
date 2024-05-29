8034892ef30021c6433d67c0b0cab531b3890c28
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-05-29 10:28:05 +0900

    code update.

---
+++ JSON_Test/src/main/java/YU/JSON_Test/JsonTestApplication.java
@@ -0,0 +1,13 @@
+package YU.JSON_Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonTestApplication.class, args);
+	}
+
+}
